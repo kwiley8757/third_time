d8a239f832ad73f9677ee1fbd08de4c9fbc5f219
Kate <kwiley8757@gmail.com>
2012-10-07 19:53:26 +0800

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThirdTime::Application.config.secret_token = '141ec335387d9335227540c8c944d263daebf8c82d7209ea0847283d3e345e635831e114d413ad6c001f29c775cc3b2e7c59a58e0cb35baab1ae0d618bb18497'
