f0c73213c23b33f7ef63df88ccc58b96f4d1ec2f
Kate <kwiley8757@gmail.com>
2012-10-07 21:49:09 +0800

    Finish exercises

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  let(:base_title) { "ThirdTime" }
 
   describe "Home page" do
 
@@ -12,7 +13,7 @@ describe "Static pages" do
     it "should have the title Home" do
   visit '/static_pages/home'
   page.should have_selector('title',
-                    :text => " | Home")
+                    :text => "#{base_title}| Home")
     end
   end
   describe "Help page" do
@@ -25,7 +26,7 @@ describe "Static pages" do
     it "should have the title Help" do
   visit '/static_pages/help'
   page.should have_selector('title',
-                    :text => "| Help")
+                    :text => "#{base_title}| Help")
     end
   end
   
@@ -38,9 +39,23 @@ describe "Static pages" do
     it "should have the title 'About'" do
   visit '/static_pages/about'
   page.should have_selector('title',
-                            :text => "| About")
+                            :text => "#{base_title}| About")
     end
+    
+    
   end
   
+  describe "Contact Page" do
+
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+    it "should have the title 'Contact'" do
+  visit '/static_pages/contact'
+  page.should have_selector('title',
+                            :text => "#{base_title}| Contact")
+    end
   
-end
+  end
+  end
